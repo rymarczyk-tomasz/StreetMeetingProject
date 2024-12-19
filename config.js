0cb9942a3f32c9715848c7e2ccc7d65198268c8a
Tomasz Rymarczyk <tomako0@gmail.com>
2024-12-19 16:49:50 +0100

    add form with sending data to google sheets and google drive

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+
+window.ENV = {
+    RECAPTCHA_SITE_KEY: "6LcfyIEqAAAAAOXSbhPpR24wcv-4oUv4BA-myTe8",
+    API_BASE_URL: "http://localhost:3000/upload",
+};
--- js/main.js
+++ js/main.js
@@ -1,35 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const nav = document.querySelector(".navbar-collapse");
-    const footerYear = document.querySelector(".footer-year");
-
-    document.addEventListener("click", () => {
-        if (nav.classList.contains("show")) {
-            nav.classList.remove("show");
-        }
-    });
-
-    const handleCurrentYear = () => {
-        const year = new Date().getFullYear();
-        footerYear.innerText = year;
-    };
-
-    handleCurrentYear();
-
-    // Dodaj śledzenie zdarzeń
-    document.querySelectorAll("a").forEach((anchor) => {
-        anchor.addEventListener("click", () => {
-            // Śledź zdarzenie kliknięcia, na przykład za pomocą Google Analytics
-            if (typeof gtag === "function") {
-                gtag("event", "click", {
-                    event_category: "link",
-                    event_label: anchor.href,
-                });
-            }
-        });
-    });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("registrationForm");
     const responseMessage = document.getElementById("responseMessage");
 
