2533e8b6aa08b2ecbc5cb3ceb1142a37f6a79756
Zenith1997 <dabarerazenith@gmail.com>
2025-02-06 23:43:14 +0530

    navbar added

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -2,8 +2,16 @@
 
 const Navbar = () => {
   return (
-    <div className='text-white'>
-      <h1 >React</h1>
+    <div className='px-4 flex flex-row  max-w-[1240px] mx-auto h-24 justify-end items-center text-white'>
+      <h1 className="w-full text-3xl font-bold  text-[#00df9a] " >React</h1>
+      <ul className="flex uppercase ">
+        <li className=" p-4">Home</li>
+        <li className="p-4">Event</li>
+        <li className="p-4">Galeria</li>
+        <li className="p-4">Formularz</li>
+        <li className="p-4">Faq</li>
+        <li className="p-4">Regulamin</li>
+      </ul>
     </div>
   )
 }
