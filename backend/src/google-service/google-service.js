65fd3a9eb5da6a491975cbbbd9ef229a1cef48d7
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2025-02-20 20:38:56 +0100

    upload to google

--- backend/src/google-service/google-service.js
+++ backend/src/google-service/google-service.js
@@ -1,42 +1,52 @@
+import dotenv from "dotenv";
 import { GoogleAuth } from "google-auth-library";
 import * as google from "googleapis";
+import fs from "fs";
 
-// Wczytanie danych uwierzytelniających z zmiennych środowiskowych
-const credentials = {
-    type: "service_account",
-    project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY
-        ? process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
-        : null,
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-    universe_domain: "googleapis.com",
-};
+export class GoogleService {
+    auth;
+    credentials;
+    sheets;
+    drive;
+    SPREADSHEET_ID;
+    GOOGLE_DRIVE_FOLDER_ID;
 
-// Konfiguracja autoryzacji Google Auth
-const auth = new GoogleAuth({
-    credentials: credentials,
-    scopes: [
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive",
-    ],
-});
+    constructor(envPath) {
+        dotenv.config({ path: envPath });
 
-const sheets = new google.sheets_v4.Sheets({ auth: auth });
-const drive = new google.drive_v3.Drive({ auth: auth });
+        this.credentials = {
+            type: "service_account",
+            project_id: process.env.GOOGLE_PROJECT_ID,
+            private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+            private_key: process.env.GOOGLE_PRIVATE_KEY
+                ? process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+                : null,
+            client_email: process.env.GOOGLE_CLIENT_EMAIL,
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            auth_uri: "https://accounts.google.com/o/oauth2/auth",
+            token_uri: "https://oauth2.googleapis.com/token",
+            auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+            client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+            universe_domain: "googleapis.com",
+        };
 
-const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
-const GOOGLE_DRIVE_FOLDER_ID = process.env.DRIVE_FOLDER_ID;
+        this.auth = new GoogleAuth({
+            credentials: this.credentials,
+            scopes: [
+                "https://www.googleapis.com/auth/spreadsheets",
+                "https://www.googleapis.com/auth/drive",
+            ],
+        });
 
-export async function appendToSheet(data) {
-    try {
-        sheets.spreadsheets.values.append({
-            spreadsheetId: SPREADSHEET_ID,
+        this.sheets = new google.sheets_v4.Sheets({ auth: this.auth });
+        this.drive = new google.drive_v3.Drive({ auth: this.auth });
+        this.SPREADSHEET_ID = process.env.SPREADSHEET_ID;
+        this.GOOGLE_DRIVE_FOLDER_ID = process.env.DRIVE_FOLDER_ID;
+    }
+
+    async appendToSheet(data) {
+        return await this.sheets.spreadsheets.values.append({
+            spreadsheetId: this.SPREADSHEET_ID,
             range: "Arkusz1!A:H",
             valueInputOption: "RAW",
             resource: {
@@ -53,43 +63,34 @@ export async function appendToSheet(data) {
                     ],
                 ],
             },
+        }).then((response) => {
+            return response.data;
+        }).catch((error) => {
+            console.error("Error appending to sheet: ", error);
         });
-    } catch (error) {
-        console.error(
-            "Błąd podczas dodawania danych do Google Sheets:",
-            error.message
-        );
-        throw new Error("Nie udało się dodać danych do arkusza.");
     }
-}
 
-export async function createUserFolder(firstName, lastName) {
-    try {
+    async createUserFolder(firstName, lastName) {
         const folderName = `${firstName} ${lastName}`;
         const folderMetadata = {
             name: folderName,
             mimeType: "application/vnd.google-apps.folder",
-            parents: [GOOGLE_DRIVE_FOLDER_ID], // Folder główny w Google Drive
+            parents: [this.GOOGLE_DRIVE_FOLDER_ID], // Folder główny w Google Drive
         };
 
-        const folder = drive.files.create({
+        const folder = await this.drive.files.create({
             resource: folderMetadata,
             fields: "id, webViewLink",
         });
 
         return { id: folder.data.id, link: folder.data.webViewLink };
-    } catch (error) {
-        console.error("Błąd przy tworzeniu folderu:", error);
-        throw new Error("Nie udało się utworzyć folderu użytkownika.");
     }
-}
 
-export async function uploadFilesToDrive(files, userFolderId) {
-    const fileLinks = [];
+    async uploadFilesToDrive(files, userFolderId) {
+        const fileLinks = [];
 
-    for (const file of files) {
-        try {
-            const uploadedFile = drive.files.create({
+        for (const file of files) {
+            const uploadedFile = await this.drive.files.create({
                 resource: {
                     name: file.originalname,
                     parents: [userFolderId], // Wysyłamy plik do folderu użytkownika
@@ -102,20 +103,7 @@ export async function uploadFilesToDrive(files, userFolderId) {
             });
 
             fileLinks.push(uploadedFile.data.webViewLink);
-        } catch (error) {
-            console.error(
-                `Błąd przy przesyłaniu pliku ${file.originalname}:`,
-                error
-            );
-        } finally {
-            fs.unlink(file.path, (err) => {
-                if (err)
-                    console.error(
-                        `Błąd przy usuwaniu pliku ${file.originalname}:`,
-                        err
-                    );
-            });
         }
+        return fileLinks;
     }
-    return fileLinks;
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,22 @@
-import multer from 'multer';
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import fs from 'fs';
-import { createUserFolder, appendToSheet, uploadFilesToDrive } from './google-service/google-service.js';
+import multer from "multer";
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { GoogleService } from "./google-service/google-service.js";
 
-console.log(process.env.NODE_ENV);
 const app = express();
 const PORT = process.env.PORT || 33000;
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 
 const __dirname = path.resolve();
 
-if (env === 'development') {
-    dotenv.config({ path: path.join(__dirname, "../config/.env_dev") });
-} else {
-    dotenv.config({ path: path.join(__dirname, "../config/.env") });
-}
+let envPath = "";
+env === 'development' ?
+    envPath = path.join(__dirname, "./.env.development") :
+    envPath = path.join(__dirname, "./.env");
+
+dotenv.config({ path: envPath });
 
 // Middleware
 app.use(cors());
@@ -25,61 +24,59 @@ app.use(express.static(path.join(__dirname, "../")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/upload", multer({dest: "./uploads/"}).array("photo"), async (req, res) => {
+const googleService = new GoogleService(envPath);
 
-    const {
-        firstName,
-        lastName,
-        email,
-        phone,
-        licensePlate,
-        carBrand,
-        carDescription,
-    } = req.body;
+app.post(
+    "/upload",
+    multer({ dest: "./uploads/" }).array("photo"),
+    async (req, res) => {
+        const {
+            firstName,
+            lastName,
+            email,
+            phone,
+            licensePlate,
+            carBrand,
+            carDescription,
+        } = req.body;
 
-    console.log(firstName);
+        if (
+            !firstName ||
+            !lastName ||
+            !email ||
+            !phone ||
+            !licensePlate ||
+            !carBrand ||
+            !carDescription ||
+            !req.files
+        ) {
+            return res.status(400).json({ message: "Wszystkie pola są wymagane." });
+        }
 
-    if (
-        !firstName ||
-        !lastName ||
-        !email ||
-        !phone ||
-        !licensePlate ||
-        !carBrand ||
-        !carDescription ||
-        !req.files
-    ) {
-        return res
-            .status(400)
-            .json({ message: "Wszystkie pola są wymagane." });
+        const userFolder = await googleService.createUserFolder(
+            firstName,
+            lastName
+        );
+        await googleService.uploadFilesToDrive(req.files, userFolder.id);
+        await googleService.appendToSheet({
+            firstName,
+            lastName,
+            email,
+            phone,
+            licensePlate,
+            carBrand,
+            carDescription,
+            photoUrls: userFolder.link, // Zapisujemy link do folderu
+        }).then((response) => response);
+
+        res.status(200)
+            .json({
+                message:
+                    "Gratulacje! Twoje zgłoszenie zostało przyjęte, niebawem odezwiemy się z decyzją :)",
+            });
+        // TODO: Delete files from '/uploads'
     }
-
-    req.files.forEach(f => {
-        console.log(f.path);
-    });
-
-    const userFolder = await createUserFolder(firstName, lastName);
-    await uploadFilesToDrive(req.files, userFolder.id);
-    await appendToSheet({
-        firstName,
-        lastName,
-        email,
-        phone,
-        licensePlate,
-        carBrand,
-        carDescription,
-        photoUrls: userFolder.link, // Zapisujemy link do folderu
-    });
-    res.json({
-        message:
-            "Gratulacje! Twoje zgłoszenie zostało przyjęte, niebawem odezwiemy się z decyzją :)",
-    });
-
-    req.files.forEach(f => {
-        fs.rm(f.path);
-    })
-});
-
+);
 
 app.listen(PORT, () => {
     console.log(`Listening on: ${PORT}`);
