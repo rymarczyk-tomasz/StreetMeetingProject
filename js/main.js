f92fcae315c0deb5e97741e560be62c5a7e79834
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2025-02-20 19:49:51 +0100

    upload form

--- js/main.js
+++ js/main.js
@@ -32,7 +32,6 @@ const responseMessage = document.getElementById("responseMessage");
 
 let dotsInterval;
 
-
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
@@ -42,28 +41,15 @@ form.addEventListener("submit", async (event) => {
 
     const formData = new FormData(form);
 
-    try {
-        const response = await fetch(window.ENV.API_BASE_URL, {
-            method: "POST",
-            body: formData,
-        });
-
-        conosle.log('response', response);
-        // clearInterval(dotsInterval); // Zatrzymanie animacji
-        if (response.ok) {
-            responseMessage.innerText = `Gratulacje! Twoje zgłoszenie zostało przyjęte, niebawem odezwiemy się z decyzją :)`;
-            responseMessage.style.color = "green";
-        } else {
-            const error = await response.json();
-            responseMessage.innerText = `Błąd: ${
-                error.message || "Wystąpił błąd przy wysyłaniu formularza."
-            }`;
-            responseMessage.style.color = "red";
-        }
-    } catch (error) {
-        // clearInterval(dotsInterval); // Zatrzymanie animacji
+    await fetch(window.ENV.API_BASE_URL, {
+        method: "POST",
+        body: formData,
+    }).then((_) => {
+        responseMessage.innerText = `Gratulacje! Twoje zgłoszenie zostało przyjęte, niebawem odezwiemy się z decyzją :)`;
+        responseMessage.style.color = "green";
+    }).catch((_) =>{
         responseMessage.innerText =
             "Wystąpił błąd przy wysyłaniu formularza. Spróbuj ponownie później.";
         responseMessage.style.color = "red";
-    }
+    });
 });
