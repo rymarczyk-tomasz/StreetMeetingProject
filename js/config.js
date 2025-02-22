5a52e4b50078466df930184f59ed1d4c584267da
Tomasz Rymarczyk <tomako0@gmail.com>
2025-02-22 19:09:01 +0100

    Update API base URL for production deployment; switch from localhost to live server

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // config.js
 
 window.ENV = {
-    // API_BASE_URL: "https://streetmeetingbackend.azurewebsites.net/upload",
-    API_BASE_URL: "http://localhost:33000/upload",
+    API_BASE_URL: "https://streetmeetingbackend.azurewebsites.net/upload",
+    // API_BASE_URL: "http://localhost:33000/upload",
 };
