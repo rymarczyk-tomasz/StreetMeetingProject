4a827bcb0d73a0c17f9efe4199e2be0d4478e7a0
Tomasz Rymarczyk <tomako0@gmail.com>
2025-02-25 18:22:21 +0100

    Remove obsolete image files and update references to use WebP format for improved performance

--- js/gallery.js
+++ js/gallery.js
@@ -13,19 +13,19 @@ document.addEventListener("DOMContentLoaded", () => {
     function loadGallery() {
         const imageFolder = "img/gallery"; // Ścieżka do folderu ze zdjęciami
         const imageNames = [
-            "1.jpg",
-            "2.jpg",
-            "3.jpg",
-            "4.jpg",
-            "5.jpg",
-            "6.jpg",
-            "7.jpg",
-            "8.jpg",
-            "9.jpg",
-            "10.jpg",
-            "11.jpg",
-            "12.jpg",
-            "13.jpg",
+            "1.webp",
+            "2.webp",
+            "3.webp",
+            "4.webp",
+            "5.webp",
+            "6.webp",
+            "7.webp",
+            "8.webp",
+            "9.webp",
+            "10.webp",
+            "11.webp",
+            "12.webp",
+            "13.webp",
         ]; // Lista nazw zdjęć - dostosuj do swoich potrzeb
 
         const fragment = document.createDocumentFragment();
