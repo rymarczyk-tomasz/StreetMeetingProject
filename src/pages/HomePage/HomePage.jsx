d6336efa68bd25de9701641d10449eba802e081a
Tomasz Rymarczyk <tomako0@gmail.com>
2025-04-24 19:37:28 +0200

    feat: implement analytics initialization in HomePage component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,6 +7,7 @@ import GallerySection from "../../components/GallerySection/GallerySection";
 import ContactSection from "../../components/ContactSection/ContactSection";
 import FormSection from "../../components/FormSection/FormSection";
 import styles from "./HomePage.module.css";
+import { initializeAnalytics } from "../../analytics";
 
 const HomePage = () => {
     const location = useLocation();
@@ -25,70 +26,7 @@ const HomePage = () => {
     });
 
     useEffect(() => {
-        if (!window.fbq) {
-            !(function (f, b, e, v, n, t, s) {
-                if (f.fbq) return;
-                n = f.fbq = function () {
-                    n.callMethod
-                        ? n.callMethod.apply(n, arguments)
-                        : n.queue.push(arguments);
-                };
-                if (!f._fbq) f._fbq = n;
-                n.push = n;
-                n.loaded = !0;
-                n.version = "2.0";
-                n.queue = [];
-                t = b.createElement(e);
-                t.async = !0;
-                t.src = v;
-                s = b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t, s);
-            })(
-                window,
-                document,
-                "script",
-                "https://connect.facebook.net/en_US/fbevents.js"
-            );
-            window.fbq("init", "2023117151421928");
-            window.fbq("track", "PageView");
-        }
-
-        if (!window.dataLayer) {
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-                window.dataLayer.push(arguments);
-            }
-            gtag("js", new Date());
-            gtag("config", "G-C740ZF1G1N");
-
-            const script = document.createElement("script");
-            script.async = true;
-            script.src =
-                "https://www.googletagmanager.com/gtag/js?id=G-C740ZF1G1N";
-            document.head.appendChild(script);
-
-            return () => {
-                if (document.head.contains(script)) {
-                    document.head.removeChild(script);
-                }
-            };
-        }
-
-        if (!window.dataLayer) {
-            (function (w, d, s, l, i) {
-                w[l] = w[l] || [];
-                w[l].push({
-                    "gtm.start": new Date().getTime(),
-                    event: "gtm.js",
-                });
-                var f = d.getElementsByTagName(s)[0],
-                    j = d.createElement(s),
-                    dl = l != "dataLayer" ? "&l=" + l : "";
-                j.async = true;
-                j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-                f.parentNode.insertBefore(j, f);
-            })(window, document, "script", "dataLayer", "GTM-WHCQDGMC");
-        }
+        initializeAnalytics();
     }, []);
 
     useEffect(() => {
