c16759aa8e596c5738b60121a19d49f9ced4884b
Tomasz Rymarczyk <tomako0@gmail.com>
2025-06-29 15:06:05 +0200

    chore: refactor deploy workflow and update Vite config for consistency

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base: "/StreetMeetingProject/",
     plugins: [react()],
-    base: '/StreetMeetingProject/',
 });
