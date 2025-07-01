137c8c8e1ba581f8e291a2954ffd5be32bd7ba25
Tomasz <tomako0@gmail.com>
2025-07-01 12:01:27 +0200

    chore: remove event details from Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,19 +6,7 @@ const Header = () => {
             <div
                 className={`${styles.heroContent} container-fluid h-100 d-flex flex-column justify-content-center align-items-center text-light text-center`}
             >
-                <h1 className="display-3 text-uppercase">Street Show 2025</h1>
-                <h2 className="mb-2 display-5 text-uppercase">31 maja</h2>
-                <h2 className="mb-2 text-uppercase">
-                    Polsat Plus Arena, Gdańsk
-                </h2>
-                <a
-                    href="https://bkb.pl/151648-edcca"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={styles.ctaButton}
-                >
-                    Kup bilet
-                </a>
+                <h1 className="display-3 text-uppercase">Street Show</h1>
                 <div className={styles.heroShadow}></div>
             </div>
         </header>
